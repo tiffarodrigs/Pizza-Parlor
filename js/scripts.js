3343a59c9686be14133a7702034713f4326c7948
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-01-30 11:32:49 -0800

    Update styles and display feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function Cart() {
   this.pizzaList = {};
   this.currentId = 0;
-  this.totalPrice =0;
+  this.totalPrice = 0;
 
 }
 
@@ -30,9 +30,6 @@ Cart.prototype.deletePizza = function(id) {
   return false;
 }
 
-
-
-
 function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
@@ -46,11 +43,11 @@ const toppingPrice = {
   onion: 3,
   cheese: 2
 }
+
 const sizePrice = {
   small: 10,
   medium: 12,
   large: 15
-
 }
 
 Pizza.prototype.calculateToppingCost = function() {
@@ -71,17 +68,16 @@ Pizza.prototype.calculateSizeCost = function() {
 Pizza.prototype.pizzaCost = function() {
   return this.calculateToppingCost() + this.calculateSizeCost();
 }
-Cart.prototype.totalCartPrice=function(pizza){
-this.totalPrice += pizza.pizzaCost();
-return this.totalPrice;
+
+Cart.prototype.totalCartPrice = function(pizza) {
+  this.totalPrice += pizza.pizzaCost();
+  return this.totalPrice;
 }
+
 function displayPizzaList(cartObj) {
-  console.log("cart object  ", cartObj);
-  console.log("cart object pizz list ", cartObj.pizzaList);
   let htmlPizzaList = ""
   Object.keys(cartObj.pizzaList).forEach(function(key) {
     let pizzaObj = cartObj.findPizza(key);
-    console.log("pizza object  ", pizzaObj);
     htmlPizzaList += "<li id=" + pizzaObj.id + ">" + "Create your own pizza : " + pizzaObj.size + "</li>";
   });
   $("ul#pizzaList").html(htmlPizzaList)
@@ -91,43 +87,33 @@ function showPizzaDetail(pizzaid) {
   let pizzaObj = cart.findPizza(pizzaid);
   $("#pizzaDetail").show();
   $(".size").html(pizzaObj.size);
-  console.log("pizzObj topp",pizzaObj.toppings.join());
   $(".topping").html(pizzaObj.toppings.join());
   $(".price").html(pizzaObj.pizzaCost());
 }
 
 function addHandler() {
   $("ul#pizzaList").on("click", "li", function() {
-
     showPizzaDetail(this.id);
   });
-
 }
+
+
 let cart = new Cart();
 $(document).ready(function() {
-
   addHandler();
   $("#addPizza").click(function(event) {
     event.preventDefault();
     let inputToppings = []
     let outputCost;
     let inputSize = $("input[name='size']:checked").val();
-    console.log(inputSize);
     $("input[name='toppings']:checked").each(function() {
       inputToppings.push($(this).val());
     });
-    console.log(inputToppings);
     let myPizza = new Pizza(inputToppings, inputSize);
     cart.addPizza(myPizza);
     displayPizzaList(cart)
-    //outputCost = myPizza.pizzaCost();
     outputCost = cart.totalCartPrice(myPizza);
-    //totalCartPrice(myPizza)
     $("#cost").html(outputCost);
     $("#resultBorder").show();
-
   });
-
-
-
 });
